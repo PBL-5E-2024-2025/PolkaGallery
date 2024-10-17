2e562a9ccd95180705c0c86e8aef542002a75ad1
Dika Muzalifah <dikanurmusdhalifah@gmail.com>
2024-10-17 14:27:45 +0700

    perbaruan lomba dan tugas akhir

--- kelolaTA.js
+++ kelolaTA.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
 async function fetchProjects() {
     const { data, error } = await supabaseClient
         .from('project')
-        .select('id_project, judul, deskripsi, tahun, jenis_project, id_tim, link_project, link_video, url_image, tools, kontak_mahasiswa')
-        .eq('jenis_project', 'PBL'); // Add this line to filter projects by 'PBL'
+        .select('id_project, judul, deskripsi, tahun, jenis_project, link_project, link_video, url_image, tools, kontak_mahasiswa')
+        .eq('jenis_project', 'TA'); // Add this line to filter projects by 'TA'
 
     if (error) {
         console.error('Error fetching projects:', error);
@@ -30,7 +30,6 @@ async function fetchProjects() {
             <td>${project.deskripsi}</td>
             <td>${project.tahun}</td>
             <td>${project.jenis_project}</td>
-            <td>${project.id_tim}</td>
             <td><a href="${project.link_project}" target="_blank">Link Project</a></td>
             <td><a href="${project.link_video}" target="_blank">Link Video</a></td>
             <td><img src="${project.url_image}" alt="Gambar Proyek" width="50"></td>
